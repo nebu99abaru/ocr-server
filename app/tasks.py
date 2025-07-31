798e48c0a5a8835374a62d548d960f2670af8e99
nebu99abaru <jhoogenboom@gmail.com>
2025-08-01 00:00:28 +0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -5,4 +5,5 @@ app = Celery("worker", broker="redis://redis:6379/0")
 
 @app.task
 def ocr_pdf(pdf_path, output_path):
-    subprocess.run(["ocrmypdf", "--force-ocr", "--sidecar", output_path, pdf_path], check=True)
+    output_pdf = "/tmp/output.pdf"  # dummy output path
+    subprocess.run(["ocrmypdf", "--force-ocr", "--sidecar", output_path, pdf_path, output_pdf], check=True)
